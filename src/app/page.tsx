98517f932bc4c9cb4807a3bdc3f16ec13fc0ea00
Brandon Chung <chung.brandon123@gmail.com>
2024-02-11 10:46:01 -0600

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ export default function Index() {
 	return (
 		<div className="flex-1 w-full flex flex-col gap-20 items-center">
 			<div className="text-center font-medium mt-32">
-				<p className="text-6xl mb-3">Build In a Weekend</p>
-				<p className="text-6xl text-secondaryText">Scale to Millions</p>
+				<p className="text-6xl mb-3">Overcome Your Struggles</p>
+				<p className="text-6xl text-secondaryText">
+					Practice with EchoAI Today
+				</p>
 			</div>
 		</div>
 	);
